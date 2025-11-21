32d6677c4c1fab86ba8c24074e469e4dde431eab
shourya <shourya.h.vardhan@gmail.com>
2025-11-20 22:12:17 -0500

    Finish Task 1.1

--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,25 @@
 package theater;
 
+/**
+ * Represents a play.
+ * @null error null
+ */
 public class Play {
 
-    public String name;
-    public String type;
+    private final String name;
+    private final String type;
 
     public Play(String name, String type) {
         this.name = name;
         this.type = type;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
 }
+
